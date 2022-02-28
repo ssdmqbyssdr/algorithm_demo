86033063d1dda9dd80eb3eb6f69ff524c8e438ff
bin yu <bin.yu@ucloud.cn>
2022-02-28 18:12:10 +0800

    add algo_demo

--- src/main/algoridemo/MaxProduct.java
+++ src/main/algoridemo/MaxProduct.java
@@ -27,7 +27,23 @@ public class MaxProduct {
         int min1 = 0,min2 = 0;
         int max1 =0 ,max2 = 0, max3 = 0;
         for (int x:nums){
-            if(x < min1)
+            if(x < min1){
+                min2 =min1;
+                min1 = x;
+            }else if (x < min2){
+                min2 = x;
+            }
+            if (x > max1){
+                max3 =max2;
+                max2 = max1;
+                max1 =x;
+            }else if (x > max2){
+                max3 = max2;
+                max2 = x;
+            }else if (x > max3){
+                max3= x;
+            }
         }
+        return Math.max(min1*min2*max1,max1*max2*max3);
     }
 }
